1b64b07e73322848e138b639e22633742c505f00
amandena <manda.panda91@yahoo.com>
2019-07-20 14:57:20 -0400

    added index prototype function

--- app/assets/javascripts/party.js
+++ app/assets/javascripts/party.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   bindClickHandlers()
 })
 
-const bindClickHandlers = () => { // costume_party index click handler
+const bindClickHandlers = () => { // index click handler
   $('.all_parties').on('click', e => {
     e.preventDefault()
     fetch('/costume_parties.json')
@@ -11,16 +11,27 @@ const bindClickHandlers = () => { // costume_party index click handler
         $('.content').html('')
         parties.forEach(party => {
           let newParty = new CostumeParty(party)
-          console.log(newParty)
+          let partyHTML = newParty.formatIndex()
+          $('.content').append(partyHTML)
         })
       })
   })
 }
 
-function CostumeParty(party) { // costume_party constructor function
+function CostumeParty(party) { // constructor function
   this.id = party.id
   this.name = party.name
   this.date = party.date
   this.time = party.time
   this.user = party.user
+}
+
+CostumeParty.prototype.formatIndex = function() { // index prototype function
+  let partyHTML = `
+    <h4>${this.name}</h4>
+    <p>${this.time} p.m.</p>
+    <p>${this.date}</p>
+    <p>Host: ${this.user.create_username}</p>
+  `
+  return partyHTML // ---> user is undefined on line 34?
 }
