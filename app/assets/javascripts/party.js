bc7f363c277bc6846f78df74ac242a441cdbb644
amandena <manda.panda91@yahoo.com>
2019-07-20 14:29:18 -0400

    added costume party constructor function

--- app/assets/javascripts/party.js
+++ app/assets/javascripts/party.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   bindClickHandlers()
 })
 
-const bindClickHandlers = () => {
+const bindClickHandlers = () => { // costume_party index click handler
   $('.all_parties').on('click', e => {
     e.preventDefault()
     fetch('/costume_parties.json')
@@ -10,8 +10,17 @@ const bindClickHandlers = () => {
       .then(parties => {
         $('.content').html('')
         parties.forEach(party => {
-          console.log(party)
+          let newParty = new CostumeParty(party)
+          console.log(newParty)
         })
       })
   })
+}
+
+function CostumeParty(party) { // costume_party constructor function
+  this.id = party.id
+  this.name = party.name
+  this.date = party.date
+  this.time = party.time
+  this.user = party.user
 }
