d97a047b38961c84c31075d77e36586b4d148ac6
amandena <manda.panda91@yahoo.com>
2019-07-20 14:08:46 -0400

    added bindClickHandlers function

--- app/assets/javascripts/party.js
+++ app/assets/javascripts/party.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
-  console.log("hello")
+  bindClickHandlers()
 })
+
+const bindClickHandlers = () => {
+  $('.all_parties').on('click', e => {
+    e.preventDefault()
+    fetch('/costume_parties.json')
+      .then(res => res.json())
+      .then(parties => {
+        $('.content').html('')
+        parties.forEach(party => {
+          console.log(party)
+        })
+      })
+  })
+}
